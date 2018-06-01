c0712990f13805aa74f3ce41411d6c1ceec50f98
terry <terry@constructioncloud.com.au>
2018-06-01 17:24:01 +1000

    refactor

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,33 +1,44 @@
-let count = 0;
+class Counter extends React.Component {
 
-const addOne = () => {
-  count++;
-  console.log(count);
-  renderCounter();
-};
 
-const minusOne = () => {
-  count--;
-  console.log(count);
-  renderCounter();
-};
+  constructor(props) {
+    super(props);
+    this.addOne = this.addOne.bind(this);
+    this.minusOne = this.minusOne.bind(this);
+    this.reset = this.reset.bind(this);
+    this.state = {
+      count: 0,
+      test: true
+    };
+  }
 
-const reset = () => {
-  count = 0;
-  console.log(count);
-  renderCounter();
-};
+  addOne() {
+    this.setState((prevState) => {
+      return {count: prevState.count + 1};
+    });
+  }
 
-const renderCounter = () => {
-  const templateTwo = (
-    <div>
-      <h1>Count: {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>Reset</button>
-    </div>
-  );
-  ReactDOM.render(templateTwo, document.getElementById('app'));
-};
+  minusOne() {
+    this.setState((prevState) => {
+      return {count: prevState.count - 1}
+    });
+  }
 
-renderCounter();
+  reset() {
+    this.setState(() => {
+      return {count: 0};
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.addOne}>+1</button>
+        <button onClick={this.minusOne}>-1</button>
+        <button onClick={this.reset}>Reset</button>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<Counter />, document.getElementById('app'));
