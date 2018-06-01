6159c2dcf8bf4b9692b5b0b4d801cdf11838b8aa
terry <terry@constructioncloud.com.au>
2018-06-01 17:06:16 +1000

    binding

--- src/app.js
+++ src/app.js
@@ -1,47 +1,89 @@
+class IndecisionApp extends React.Component {
+  render() {
+    const title = 'test';
+    const subTitle = 'test2';
+    const options = [1, 2, 3];
+    return (
+      <div>
+        <Header title={title} subTitle={subTitle}/>
+        <Action />
+        <Options options={options}/>
+        <AddOption />
+      </div>
+    );
+  }
+}
 class Header extends React.Component {
   render() {
     return (
-      <p>test</p>
+      <div>
+        <p>{this.props.title}</p>
+        <p>{this.props.subTitle}</p>
+      </div>
     );
   }
 }
 
 class Action extends React.Component {
+  handlePick() {
+    console.log(1)
+  }
+
   render() {
     return (
       <div>
-        <button type="button">What should I do?</button>
+        <button onClick={this.handlePick} type="button">What should I do?</button>
       </div>
     );
   }
 }
 
 class Options extends React.Component {
+  constructor(props) {
+    super(props);
+    this.removeAll = this.removeAll.bind(this)
+  }
+
+  removeAll() {
+    console.log(this.props.options)
+  }
+
   render() {
     return (
-      <ol>
-        <li>1</li>
-      </ol>
+      <div>
+        {this.props.options.map((option, index) => <Option key={index} value={option}/>)}
+        <button onClick={this.removeAll}>Remove all</button>
+      </div>
+    );
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return (
+      <div>{this.props.value}</div>
     );
   }
 }
 
 class AddOption extends React.Component {
+  onSubmit(e) {
+    e.preventDefault();
+    const option = e.target.elements.option.value.trim();
+    console.log(option)
+    e.target.elements.option.value = '';
+  }
+
   render() {
     return (
-      <button type="button">Add Option</button>
+      <form onSubmit={this.onSubmit}>
+        <input type="text" name="option"/>
+        <button type="submit">Add Option</button>
+      </form>
     );
   }
 }
 
-const jsx = (
-  <div>
-    <Header />
-    <Action />
-    <Options/>
-    <AddOption />
-  </div>
-);
 
 const appRoot = document.getElementById('app');
-ReactDOM.render(jsx, appRoot);
+ReactDOM.render(<IndecisionApp/>, appRoot);
