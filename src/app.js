5e374246c1bf646a96bfc3c3aa0d3b12f7c7d280
terry <terry@constructioncloud.com.au>
2018-06-18 17:36:21 +1000

    scss

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import ReactDom from 'react-dom';
 import React from 'react';
 import IndecisionApp from './components/IndecisionApp';
+import  './styles/styles.scss';
 
 ReactDom.render(
   <IndecisionApp options={['one', 'two']}/>, document.getElementById('app'));
