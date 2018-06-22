04f354b5f110bc8f34985ec10591f2a0eb289db5
terry <terry@constructioncloud.com.au>
2018-06-22 16:52:56 +1000

    more styles

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Modal from 'react-modal';
 
 const OptionModal = (props) => (
-  <Modal onRequestClose={props.handleClearSelectedOption} isOpen={!!props.selectedOption}
-         contentLabel="Selected Option">
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleClearSelectedOption}>OK</button>
+  <Modal onRequestClose={props.handleClearSelectedOption}
+         isOpen={!!props.selectedOption}
+         contentLabel="Selected Option"
+         closeTimeoutMS={200}
+         className="modal">
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+    <button className="button" onClick={props.handleClearSelectedOption}>OKay</button>
   </Modal>
 );
 
